236efe8bf41fa5f5f64d7c122ed98bf0ab4ff526
dfabian <dfabian@qualivision.ch>
2021-11-19 11:41:32 +0100

    Registered qt resource files of the module

--- XmlDomWrapper.h
+++ XmlDomWrapper.h
@@ -1,6 +1,7 @@
 #ifndef XMLDOMWRAPPER_H
 #define XMLDOMWRAPPER_H
 
+#include <QCoreApplication>
 #include <QQmlEngine>
 #include <QtGlobal>
 
@@ -9,6 +10,12 @@
 #else
   #define DLLDOMWRAPPER Q_DECL_IMPORT
 #endif
+
+inline void InitResources()
+{
+  Q_INIT_RESOURCE(xmlDomResources);
+}
+Q_COREAPP_STARTUP_FUNCTION(InitResources)
 
 namespace xmldom {
 inline void RegisterWrapper(QQmlEngine* pEngine)
